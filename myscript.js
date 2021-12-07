b558ab2256eb14e6e0b0ccf030e28ad81a377dab
vywebsecdefenderclubq2re <95683875+vywebsecdefenderclubq2re@users.noreply.github.com>
2021-12-07 10:52:35 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 window.onload = function(){
   
  document.getElementsByTagName("body")[0].style.backgroundColor="red"; 
- document.getElementsByTagName("h3")[0].text="pnwed"; 
+ document.getElementsByTagName("h3")[0].innerHtml="pnwed"; 
 
 }
